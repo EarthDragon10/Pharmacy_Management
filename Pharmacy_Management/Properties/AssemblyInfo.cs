73c04c410506561e2afaa6f885c2ec8655ac5447
EarthDragon10 <lagudadev@gmail.com>
2023-01-30 15:23:58 +0100

    Aggiungere i file di progetto.

---
+++ Pharmacy_Management/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Le informazioni generali relative a un assembly sono controllate dal seguente
+// set di attributi. Modificare i valori di questi attributi per modificare le informazioni
+// associate a un assembly.
+[assembly: AssemblyTitle("Pharmacy_Management")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Pharmacy_Management")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Se si imposta il valore di ComVisible su falso, i tipi nell'assembly non sono più visibili
+// dai componenti COM. Se è necessario accedere al tipo nell'assembly da
+// COM, impostare su true l'attributo ComVisible per tale tipo.
+[assembly: ComVisible(false)]
+
+// Se il progetto viene esposto a COM, il GUID seguente verrà utilizzato per creare l'ID della typelib
+[assembly: Guid("9744d0b4-a92a-480b-bf8c-b8536b025fb3")]
+
+// Le informazioni sulla versione di un assembly sono costituite dai quattro valori seguenti:
+//
+//      Versione principale
+//      Versione secondaria
+//      Numero build
+//      Revisione
+//
+// È possibile specificare tutti i valori o lasciare i valori predefiniti per Revisione e Numeri build
+// utilizzando l'asterisco (*) come illustrato di seguito:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
