56b0d36b160763eb9df65762884bc86e96b06d20
EarthDragon10 <lagudadev@gmail.com>
2023-02-01 14:17:44 +0100

    fix login html

--- Pharmacy_Management/Models/Users.cs
+++ Pharmacy_Management/Models/Users.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace Pharmacy_Management.Models
 {
-    public class Users
+    public partial class Users
     {
+        [NotMapped()]
         public string Username { get; set; }
+        [NotMapped()]
         public string Password { get; set; }
     }
 }
